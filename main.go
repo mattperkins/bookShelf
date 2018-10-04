0c782b9320de559d6e6f05763d6543f7fa8ea691
mattperkins <github@mattperkins.com>
2018-10-04 14:25:37 +0100

    route handleFunc to index template

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func main() {
+	templates := template.Must(template.ParseFiles("templates/index.html"))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Test build...")
+		if err := templates.ExecuteTemplate(w, "index.html", nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		// fmt.Fprintln(w, "Test build...")
 	})
 	fmt.Println(http.ListenAndServe(":8080", nil))
 }
